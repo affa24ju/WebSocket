a5129c877599c0c94ca682eccaf549a6d06b0472
Fatema Afroza <fafroza2017@gmail.com>
2025-08-26 12:41:33 +0200

    chat endpoint

---
+++ WebSocket/src/main/java/com/WebSocket/WebSocket/Chat.java
@@ -0,0 +1,18 @@
+package com.WebSocket.WebSocket;
+
+public class Chat {
+    private String chat;
+
+    public Chat(String chat) {
+        this.chat = chat;
+    }
+
+    public String getChat() {
+        return chat;
+    }
+
+    public void setChat(String chat) {
+        this.chat = chat;
+    }
+
+}
--- WebSocket/src/main/java/com/WebSocket/WebSocket/StompController.java
+++ WebSocket/src/main/java/com/WebSocket/WebSocket/StompController.java
@@ -11,7 +11,15 @@ public class StompController {
     @SendTo("/topic/greetings")
     public Hello hello(HelloMessage message) {
         System.out.println("/hello endpoint hit with message: " + message.getName());
-        return new Hello("Hello, " + message.getName() + "!");
+        // message.getName() contains the name sent by the client from frontend
+        return new Hello("Hello! " + message.getName() + "has joined!");
+    }
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/chat")
+    public Chat chat(ChatMessage chat) {
+        System.out.println("/chat endpoint hit with message: " + chat.getContent());
+        return new Chat(chat.getContent());
     }
 
 }
